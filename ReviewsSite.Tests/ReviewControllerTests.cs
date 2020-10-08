e2b2c98fecfd90ccc0063b53e303f70aad07d816
Brian Yuhas <bcyuhas@gmail.com>
2020-10-08 10:31:31 -0400

    started building out tests and controller eclass, watching along with video

---
+++ ReviewsSite.Tests/ReviewControllerTests.cs
@@ -0,0 +1,20 @@
+using ReviewsSite.Controllers;
+using System;
+using Xunit;
+
+namespace ReviewsSite.Tests
+{
+    public class ReviewControllerTests
+    {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            //Arrange
+            ReviewController sut = new ReviewController();
+            //Act
+
+            //Assert
+             
+        }
+    }
+}
