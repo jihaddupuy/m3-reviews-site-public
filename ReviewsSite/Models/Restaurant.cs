425a01ef64d20ecf4774d2a09ccd3ba3b01e59b3
Brian Yuhas <bcyuhas@gmail.com>
2020-10-12 13:39:22 -0400

    created new properties and methods for restaurant class

--- ReviewsSite/Models/Restaurant.cs
+++ ReviewsSite/Models/Restaurant.cs
@@ -10,15 +10,21 @@ namespace ReviewsSite.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public Restaurant()
         {
 
         }
-        public Restaurant(int id, string name)
+        public Restaurant(int id, string name, string description)
         {
             Id = id;
             Name = name;
+            Description = description;
         }
+
+
+
+        
 
         
     }
--- ReviewsSite/Repositories/RestaurantRepository.cs
+++ ReviewsSite/Repositories/RestaurantRepository.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using ReviewsSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ReviewsSite.Repositories
 {
-    public class RestuarantRepository
+    public class RestaurantRepository
     {
+        public List<Restaurant> restaurantList;
+        public RestaurantRepository()
+        {
+            restaurantList = new List<Restaurant>()
+            {
+                new Restaurant(1, ""),
+                new Restaurant(2, ""),
+                new Restaurant(3, "")
+            }
+        }
+
     }
 }
