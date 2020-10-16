499a560178942cf948a5d729798fa75e368142e9
Hugh McGee <hugh41971@gmail.com>
2020-10-16 10:57:34 -0400

    created reviews repository and reviews controller

--- ReviewsSite/Startup.cs
+++ ReviewsSite/Startup.cs
@@ -29,6 +29,7 @@ namespace ReviewsSite
             services.AddMvc();
             services.AddDbContext<DiningContext>();
             services.AddScoped<IRepository<Restaurant>, RestaurantRepository>();
+            services.AddScoped<IRepository<Reviews>, ReviewsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
