3bdcb76c8e1541e3231c6aeb1c0c127dae743b5b
Hugh McGee <hugh41971@gmail.com>
2020-10-16 11:57:26 -0400

    connected index to details page

--- ReviewsSite/Repositories/ReviewsRepository.cs
+++ ReviewsSite/Repositories/ReviewsRepository.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using ReviewsSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ReviewsSite.Repositories
 {
-    public class ReviewsRepository
+    public class ReviewsRepository : IRepository<Reviews>
     {
+        private DiningContext db;
+        public ReviewsRepository(DiningContext db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<Reviews> GetAll()
+        {
+            return db.Reviews;
+        }
+        public Reviews GetById(int id)
+        {
+            return db.Reviews.Single(c => c.Id == id);
+        }
+
     }
 }
